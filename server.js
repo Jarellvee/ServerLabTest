8a31519424694af72680d02297e03c54686a4054
Jarell Vicencio <mjnvicencio@gmail.com>
2025-09-28 11:38:49 -0700

    call fucntion by handling the promise

--- server.js
+++ server.js
@@ -9,15 +9,37 @@ const server = http.createServer((req, res) => {
     const path = parsedUrl.pathname;
     const query = parsedUrl.query;
     const name = query.name;
+    const text = query.text;
 
-    if (path === '/COMP4537/labs/3/getDate/' || path === '/COMP4537/labs/3/getDate') {
+    if (path.startsWith('/COMP4537/labs/3/getDate/') || path.startsWith('/COMP4537/labs/3/getDate')) { // PT 2
         res.writeHead(200, { 'Content-Type': 'text/html' }); // to tell that were sending html not plaintxt
         const msg = fetchDate(name); // calls the function from utils.js
         res.end(`<p style="color:blue">${msg}</p>`);
-    } else {
-        res.writeHead(404, { 'Content-Type': 'text/html' });
-        res.end(`<p style="color:red">${MESSAGES.NOT_FOUND}</p>`);
+
+
+    } else if (path.startsWith('/COMP4537/labs/3/writeFile') || path.startsWith('/COMP4537/labs/3/writeFile/')) { // PT 3.A
+        if (text) {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            appendToFile(text)
+                .then(() => {
+                    res.end(`<p style="color:blue">${MESSAGES.FILE_WRITTEN}</p>`);
+                })
+                .catch(() => {
+                    res.end(`<p style="color:red">${MESSAGES.FILE_WRITE_ERROR}</p>`);
+                });
+        } else {
+            res.writeHead(400, { 'Content-Type': 'text/html' });
+            res.end(`<p style="color:red">${MESSAGES.NO_WRITE_MSG}</p>`);
+        }
+
+
+    } else if (path.startsWith('/COMP4537/labs/3/readFile/') || path.startsWith('/COMP4537/labs/3/readFile')) { // PT 3.B
+
     }
+
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.end(`<p style="color:red">${MESSAGES.NOT_FOUND}</p>`);
+
 });
 
 server.listen(PORT, () => {
