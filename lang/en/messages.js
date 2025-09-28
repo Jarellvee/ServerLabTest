5bc5e6c33ad3c6c2a429414ea216acf4bd0f0b68
Jarell Vicencio <mjnvicencio@gmail.com>
2025-09-28 12:22:46 -0700

    update message

--- lang/en/messages.js
+++ lang/en/messages.js
@@ -4,6 +4,7 @@ const MESSAGES = {
     NOT_FOUND: "404 Not Found, Check the URL and try again",
     FILE_WRITTEN: "File writing was successful, check with readFile endpoint",
     NO_WRITE_MSG: "No input provided, please include an input in the query",
-    FILE_WRITE_ERROR: "There was an error writing to the file, please try again"
+    FILE_WRITE_ERROR: "There was an error writing to the file, please try again",
+    FILE_NOT_FOUND: "The file {filename} was not found, ensure the file name is file.txt"
 };
 module.exports = MESSAGES;
