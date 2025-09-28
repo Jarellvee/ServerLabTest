9360ec130d05f91ef8815331445c32b917735736
Jarell Vicencio <mjnvicencio@gmail.com>
2025-09-28 11:39:04 -0700

    add error message

--- lang/en/messages.js
+++ lang/en/messages.js
@@ -1,6 +1,9 @@
 const MESSAGES = {
     NAME_PROVIDED: "Hello {name}, What a beautiful day. Server current date and time is {date}.",
     NO_NAME: "No name provided, please include a name in the query",
-    NOT_FOUND: "404 Not Found, Check the URL and try again"
+    NOT_FOUND: "404 Not Found, Check the URL and try again",
+    FILE_WRITTEN: "File writing was successful, check with readFile endpoint",
+    NO_WRITE_MSG: "No input provided, please include an input in the query",
+    FILE_WRITE_ERROR: "There was an error writing to the file, please try again"
 };
 module.exports = MESSAGES;
