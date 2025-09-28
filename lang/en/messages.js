4a9d689f2ea86a5f7f846bb13cfbc2a90e52c8c7
Jarell Vicencio <mjnvicencio@gmail.com>
2025-09-28 10:59:18 -0700

    separate user facing messages

---
+++ lang/en/messages.js
@@ -0,0 +1,6 @@
+const MESSAGES = {
+    NAME_PROVIDED: "Hello {name}, What a beautiful day. Server current date and time is {date},  {time} PST.",
+    NO_NAME: "No name provided, please include a name in the query",
+    NOT_FOUND: "404 Not Found"
+};
+module.exports = MESSAGES;
